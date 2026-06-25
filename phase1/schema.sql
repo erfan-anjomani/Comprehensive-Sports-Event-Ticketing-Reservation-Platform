c82b1803aded69a6cec9bd8e80cf3bfeb46e6d6c
Erfan <erfananjomani4@gmail.com>
2026-06-25 17:31:12 +0330

    add football , volleyball , basketball tables to schema

--- phase1/schema.sql
+++ phase1/schema.sql
@@ -13,11 +13,11 @@ CREATE TABLE users (
 	city VARCHAR(100) NOT NULL,
      password_hash VARCHAR(255) NOT NULL,
      register_date TIMESTAMP NOT NULL DEFAULT NOW(),
-     account_condition account_status NOT NULL DEFAULT('active'),
+     account_status account_status NOT NULL DEFAULT('active'),
 
 
      CONSTRAINT check_contact CHECK (email IS NOT NULL OR phone IS NOT NULL)
-)
+);
 
 --index:
 CREATE INDEX idx_user_city ON users(city);
@@ -33,7 +33,11 @@ WHERE phone IS NOT NULL;
 
 
 --ticket
-CREATE TYPE sport_type AS ENUM ('football','volleyball', 'basketball');
+CREATE TYPE sport_type AS ENUM (
+    'football',
+    'volleyball',
+    'basketball'
+);
 
 CREATE TYPE seat_type AS ENUM (
     'regular',
@@ -50,12 +54,10 @@ CREATE TABLE tickets (
 	match_location VARCHAR(100) NOT NULL,
      ticket_price NUMERIC(10,2) NOT NULL CHECK(ticket_price >= 0),
      remaining_capacity INTEGER NOT NULL CHECK(remaining_capacity >= 0),
-     seat seat_type NOT NULL DEFAULT('regular'),
-
 
      CONSTRAINT 
      match_check CHECK (host_team <> guest_team)
-)
+);
 
 --indexes
 CREATE INDEX idx_ticket_match_date
@@ -73,15 +75,15 @@ CREATE TYPE reservation_status_enum AS ENUM (
 );
 CREATE TABLE reservations (
      id BIGSERIAL NOT NULL PRIMARY KEY,
-     user_id BIGINT NOT NULL REFERENCES users (id),
+     user_id BIGINT NOT NULL REFERENCES users (id) ON DELETE RESTRICT,
      ticket_id BIGINT NOT NULL REFERENCES tickets (id),
      reservation_time TIMESTAMP NOT NULL ,
      reservation_status reservation_status_enum NOT NULL DEFAULT ('reserved'),
-     expiration_time TIMESTAMP NOT NULL
+     expiration_time TIMESTAMP NOT NULL,
 
      CONSTRAINT reservation_time_check
      CHECK(expiration_time > reservation_time)
-)    
+);    
 
 --indexes
 CREATE INDEX idx_reservation_user
@@ -109,13 +111,14 @@ CREATE TYPE payment_status_enum AS ENUM (
 
 CREATE TABLE payments (
      id BIGSERIAL NOT NULL PRIMARY KEY,
-     user_id BIGINT NOT NULL REFERENCES users (id),
-     reservation_id BIGINT NOT NULL REFERENCES reservations (id),
-     amount NUMERIC(10,2) NOT NULL CHECK(amount >= 0),
+     user_id BIGINT NOT NULL REFERENCES users (id) ON DELETE RESTRICT,
+     reservation_id BIGINT NOT NULL 
+     REFERENCES reservations (id) ON DELETE RESTRICT,
+     amount NUMERIC(10,2) NOT NULL CHECK(amount > 0),
      method payment_method NOT NULL,
      payment_status payment_status_enum NOT NULL DEFAULT ('pending'),
      payment_time TIMESTAMP NOT NULL DEFAULT NOW()
-)  
+);  
 
 
 --indexes
@@ -142,13 +145,14 @@ CREATE TYPE report_status_enum AS ENUM (
 
 CREATE TABLE reports (
      id BIGSERIAL NOT NULL PRIMARY KEY,
-     user_id BIGINT NOT NULL REFERENCES users (id),
-     reservation_id BIGINT NOT NULL REFERENCES reservations (id),
+     user_id BIGINT NOT NULL REFERENCES users (id) ON DELETE RESTRICT,
+     reservation_id BIGINT NOT NULL 
+     REFERENCES reservations (id) ON DELETE RESTRICT,
      category report_category NOT NULL,
      report_description TEXT NOT NULL,
      report_status report_status_enum NOT NULL DEFAULT('pending'),
      created_at TIMESTAMP NOT NULL DEFAULT NOW()
-)
+);
 
 --indexes
 CREATE INDEX idx_user_report
@@ -159,3 +163,55 @@ ON reports(report_status);
 
 CREATE INDEX idx_created
 ON reports(created_at);
+
+
+--football
+CREATE TABLE football_details (
+     id BIGSERIAL NOT NULL PRIMARY KEY,
+     ticket_id BIGINT NOT NULL UNIQUE  REFERENCES tickets (id) ON DELETE CASCADE,
+     league_name VARCHAR(255) NOT NULL,
+     stadium_name VARCHAR(255) NOT NULL,
+     row_number INTEGER NOT NULL,
+     seat_number INTEGER NOT NULL,
+     ticket_category seat_type NOT NULL,
+     facilities TEXT
+);
+
+--indexes
+CREATE INDEX idx_football_ticket
+ON football_details(ticket_id);
+
+
+--volleyball
+CREATE TABLE volleyball_details (
+    id BIGSERIAL PRIMARY KEY,
+    ticket_id BIGINT NOT NULL UNIQUE REFERENCES tickets(id) ON DELETE CASCADE,
+    league_name VARCHAR(255) NOT NULL,
+    arena_name VARCHAR(255) NOT NULL,
+    row_number INTEGER NOT NULL,
+    seat_number INTEGER NOT NULL,
+    ticket_category seat_type NOT NULL,
+    facilities TEXT
+);
+
+--indexes
+CREATE INDEX idx_volleyball_ticket
+ON volleyball_details(ticket_id);
+
+
+--basketball
+CREATE TABLE basketball_details (
+    id BIGSERIAL PRIMARY KEY,
+    ticket_id BIGINT NOT NULL UNIQUE REFERENCES tickets(id) ON DELETE CASCADE,
+    league_name VARCHAR(255) NOT NULL,
+    arena_name VARCHAR(255) NOT NULL,
+    row_number INTEGER NOT NULL,
+    seat_number INTEGER NOT NULL,
+    ticket_category seat_type NOT NULL,
+    facilities TEXT
+);
+
+
+--indexes
+CREATE INDEX idx_basketball_ticket
+ON basketball_details(ticket_id);
